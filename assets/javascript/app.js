4cb5d56215e52bd99f9c077ac22dd629bd12aed4
Lawrence Friedman <Lawrenceh.Friedman@gmail.com>
2020-05-08 14:19:01 -0400

    the last dance

---
+++ assets/javascript/app.js
@@ -0,0 +1,139 @@
+var counter = 30;
+var currentQuestion = 0;
+var score = 0;
+var lost = 0;
+var timer;
+
+
+function nextQuestion() {
+    var isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver) {
+        displayResults();
+    } else {
+        currentQuestion++;
+        loadQuestion();
+    }
+}
+
+function timeUp() {
+    clearInterval(timer);
+    lost++;
+    nextQuestion();
+}
+
+function countDown() {
+    counter--;
+
+    $('#timer').html(counter);
+
+    if (counter === 0) {
+        timeUp();
+    }
+}
+
+
+function loadQuestion() {
+    counter = 30;
+    timer = setInterval(countDown, 1000);
+    
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].answers;
+    
+    $('#timer').html(counter);
+    $('#game').html(`
+        <h4>${question}</h4>
+        ${loadChoices(choices)}
+        `);
+
+}
+
+function loadChoices(choices) {
+    let result = '';
+
+    for (i = 0; i < choices.length; i++) { 
+        result += `<p class='choice' data-answer='${choices[i]}'>${choices[i]}</p>`;
+    }
+
+    return result;
+}
+
+$(document).on('click','.choice', function(){
+    clearInterval(timer);
+    var selectedAnswer = $(this).attr('data-answer');
+    var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (correctAnswer === selectedAnswer) {
+        score++;
+        nextQuestion();
+    }
+    else {
+        lost++;
+        nextQuestion();
+    }
+
+});
+
+function displayResults() {
+    var result = `
+    <p class="summary">Correct answers: ${score}</p>
+    <p class="summary">Incorrect answers: ${lost}</p>
+    <p class="summary">Total questions: ${quizQuestions.length}</p>
+    <button class="btn btn-danger" id="reset">Reset Game</button>
+    `;
+    $('#timer').remove();
+
+    $('#game').html(result);
+
+}
+
+$(document).on('click', '#reset', function() {
+    counter = 30;
+    currentQuestion = 0;
+    score = 0;
+    lost = 0;
+    timer = null;
+
+    loadQuestion();
+});
+
+
+$('#startButton').click(function() {
+    $('#startButton').remove();
+    $('#timer').html(counter);
+    loadQuestion();
+    
+});
+
+
+const quizQuestions = [
+    {
+        question: 'Where did Michael Jordan go to college?',
+        answers: ['Duke', 'UNC', 'Texas', 'Villanova'],
+        correctAnswer: 'UNC',
+    },
+    {
+        question: 'What number pick was Michael Jordan?',
+        answers: ['First', 'Second', 'Third', 'Undrafted'],
+        correctAnswer: 'Third',
+    },
+    {
+        question: 'Who is Michael Jordans sneaker deal with?',
+        answers: ['Nike', 'Adidas', 'Converse', 'Under Armor'],
+        correctAnswer: 'Nike',
+    },
+    {
+        question: "What team was Scottie Pippen drafted by?",
+        answers: ['New York Knicks', 'Chicago Bulls', 'Seattle Supersonics', 'Miami Heat'],
+        correctAnswer: 'Seattle Supersonics',
+    },
+    {
+        question: 'Who coached the Chicago Bulls before Phil Jackson?',
+        answers: ['Pat Riley', 'Doug Collins', 'Dean Smith', 'Chuck Daly'],
+        correctAnswer: 'Doug Collins',
+    },
+    {
+        question: 'Which Season did the Bulls with their first title (in the Jordan Era)',
+        answers: ['1990', '1991', '1992', '1993'],
+        correctAnswer: '1991',
+    }
+];
